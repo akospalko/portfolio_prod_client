3d6002772ead7e09041960ffae5075adf9e41bef
Agathius <p.akos1148@gmail.com>
2023-07-15 10:26:48 +0200

    Added dynamically changing (with content size) rounded border radius to project cards. Created separate component to store its logic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,38 +6,44 @@ import Anchor from './Anchor'
 import ProjectCard from './ProjectCard';
 import { projectCards } from '../helper/dataControl'
 import { useModalContext } from '../context/ModalContext';
+import { useMediaQuery } from 'react-responsive';
+import { addConditionalBorderStyle } from './addConditionalBorderStyle';
 
 export default function Projects({ pageLayout }) {
+  // HOOK
+  const isBelow1024Px = useMediaQuery({ query: '(max-width: 1023px)' });
+  const isBelow768Px = useMediaQuery({ query: '(max-width:767px)' });
+
   // CONTEXT 
   const { toggleModalHandler } = useModalContext();
 
   // STYLE
   // conditional layout
-  let componentContainer = pageLayout === 'fullContentPage';
+  const componentContainer = pageLayout === 'fullContentPage';
 
   return (
-    <>
-      <article className={ `shared-page-container ${ componentContainer ? 'shared-page-container--minheight' : '' }` }>
-        {/* anchor tag for full content page */}
-        { componentContainer && <Anchor componentName='projects' /> }
-        <div className='projects-content'>
-          {/* Page title */}
-          <div className='shared-title'>
-            <h1> Projects </h1>
-          </div>
-          <div className='projects-display'>
-            { projectCards.map(card => (
-              <div 
-                key={ card.id }
-                onClick={ e => toggleModalHandler(e, card.id) }  
-                className='projects-card-wrapper'
-              > 
-                <ProjectCard data={{ ...card }} /> 
-              </div>
-            )) }
-          </div> 
+    <article className='shared-page-container'>
+      {/* anchor tag for full content page */}
+      { componentContainer && <Anchor componentName='projects' /> }
+      <div className='projects-content'>
+        {/* page title */}
+        <div className='shared-title'>
+          <h1> Projects </h1>
         </div>
-      </article>
-    </>
+        <div className='projects-display'>
+          { projectCards.map( (card, i) => {
+            let columnCount = isBelow1024Px ? isBelow768Px ? 1 : 2 : 3; // number of displaye columns: 1-3
+            let borderRadiusStyle = addConditionalBorderStyle(projectCards, i, columnCount);
+            return <div 
+              key={ card.id }
+              onClick={ e => toggleModalHandler(e, card.id) }  
+              className='projects-card-wrapper'
+            > 
+              <ProjectCard data={{ ...card }} borderRadiusStyle={ borderRadiusStyle } /> 
+            </div>
+          }) }
+        </div> 
+      </div>
+    </article>
   )
 }
---
+++ src/components/addConditionalBorderStyle.jsx
@@ -0,0 +1,70 @@
+// TODO: outsource css from project.css
+// Project cards: add custom border radii for different items at different grid locations   
+export const addConditionalBorderStyle = (array, i, columnCount) => {
+  if (array.length === 0) return {};
+
+  const index = i + 1;
+  const borderRadiusStyle = { item: '', itemImg: '' };
+
+  if (array.length === 1) {
+    borderRadiusStyle.item = 'border-radius-full';
+    borderRadiusStyle.itemImg = 'border-radius-bottom';
+  } else if (columnCount === 1) {
+    if (index === 1) {
+      borderRadiusStyle.item = 'border-radius-top';
+    } else if (index === array.length) {
+      borderRadiusStyle.item = 'border-radius-bottom';
+      borderRadiusStyle.itemImg = 'border-radius-bottom';
+    }
+  } else if (array.length <= columnCount) {
+    if (index === 1) {
+      borderRadiusStyle.item = 'border-radius-left';
+      borderRadiusStyle.itemImg = 'border-radius-bottom-left';
+    } else if (index === array.length) {
+      borderRadiusStyle.item = 'border-radius-right';
+      borderRadiusStyle.itemImg = 'border-radius-bottom-right';
+    }
+  } else {
+    if (columnCount === 3) {
+      if (index === 1) {
+        borderRadiusStyle.item = 'border-radius-top-left';
+      } else if (index === 3) {
+        borderRadiusStyle.item = 'border-radius-top-right';
+      } else if (index === array.length) {
+        const remainder = array.length % 3;
+        if (remainder === 1) {
+          borderRadiusStyle.item = 'border-radius-bottom-left';
+          borderRadiusStyle.itemImg = 'border-radius-bottom-left';
+        } else if (remainder === 0) {
+          borderRadiusStyle.item = 'border-radius-bottom-right';
+          borderRadiusStyle.itemImg = 'border-radius-bottom-right';
+        }
+      } else if (index === array.length - 1 && array.length % 3 === 2) {
+        borderRadiusStyle.item = 'border-radius-bottom-left';
+        borderRadiusStyle.itemImg = 'border-radius-bottom-left';
+      } else if (index === array.length - 2 && array.length % 3 === 0) {
+        borderRadiusStyle.item = 'border-radius-bottom-left';
+        borderRadiusStyle.itemImg = 'border-radius-bottom-left';
+      }
+    } else if (columnCount === 2) {
+      if (index === 1) {
+        borderRadiusStyle.item = 'border-radius-top-left';
+      } else if (index === 2) {
+        borderRadiusStyle.item = 'border-radius-top-right';
+      } else if (index === array.length) {
+        if (array.length % 2 !== 0) {
+          borderRadiusStyle.item = 'border-radius-bottom-left';
+          borderRadiusStyle.itemImg = 'border-radius-left';
+        } else {
+          borderRadiusStyle.item = 'border-radius-bottom-right';
+          borderRadiusStyle.itemImg = 'border-radius-right';
+        }
+      } else if (index === array.length - 1 && array.length % 2 === 0) {
+        borderRadiusStyle.item = 'border-radius-bottom-left';
+        borderRadiusStyle.itemImg = 'border-radius-left';
+      }
+    }
+  }
+
+  return borderRadiusStyle;
+};
