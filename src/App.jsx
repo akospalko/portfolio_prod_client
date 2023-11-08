fa61097394e14d32a2b99a9729b4c4d38fd8d6a1
Agathius <p.akos1148@gmail.com>
2023-11-08 22:53:03 +0100

    Initialized internationalization using react i18next, created en - hu translations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-// component holding context providers and page layout
+// Component holding context providers and page layout
 import './App.css';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import ModalProvider from './context/ModalContext';
 import PageLayout from './PageLayout';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
+import enTranslation from './locales/en.json';
+import huTranslation from './locales/hu.json';
+
+// Init locales
+i18n.init({
+  interpolation: { escapeValue: false }, 
+  lng: 'en', // default language
+  resources: {
+    en: { translation: enTranslation },
+    hu: { translation: huTranslation }
+  },
+});
 
 function App() {
   return (
-    <div className='layout-container' >
-      <ModalProvider>
-      <GoogleReCaptchaProvider reCaptchaKey={ import.meta.env.VITE_CAPTCHA_V3_SITE_KEY }>
-            {/* Page layout */}
-            <PageLayout/>
-      </GoogleReCaptchaProvider>
-      </ModalProvider>
+    <div className='layout-container'>
+      <I18nextProvider i18n={i18n}>
+        <ModalProvider>
+        <GoogleReCaptchaProvider reCaptchaKey={ import.meta.env.VITE_CAPTCHA_V3_SITE_KEY }>
+          <PageLayout/>
+        </GoogleReCaptchaProvider>
+        </ModalProvider>
+      </I18nextProvider>
     </div>
   )
 }
