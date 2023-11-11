df97b718e674bffe5bf175c15c83ee369c4b7d2c
Agathius <p.akos1148@gmail.com>
2023-11-11 23:39:12 +0100

    removed initial page loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,15 @@ import PageLayout from './PageLayout';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './helper/i18nInitialization';
 import './App.css';
-import PageLoader from './components/PageLoader';
-import { useState, useEffect } from 'react';
-import './index.css';
 
 function App() {
-  // STATE
-  const [ isPageLoaded, setPageLoaded ] = useState(false);
-
-  // EFFECT
-  useEffect(() => {
-    const handleLoad = () => {
-      setTimeout(() => {
-        setPageLoaded(true);
-      }, 200 )
-    };
-
-    window.addEventListener('load', handleLoad);
-    return () => { window.removeEventListener('load', handleLoad) };
-  }, []);
 
   return (
     <>
       <I18nextProvider i18n={ i18n }>
       <ModalProvider>
       <GoogleReCaptchaProvider reCaptchaKey={ import.meta.env.VITE_CAPTCHA_V3_SITE_KEY }>
-       
-        {/* <PageLayout/> */}
-  
-        {!isPageLoaded ? 
-          <PageLoader style='shared-page-container--initial-loader'/> 
-          : 
-          ( <div className='layout-container'>
-                  <PageLayout/>
-            </div> 
-          )
-        }
+        <PageLayout/>
       </GoogleReCaptchaProvider>
       </ModalProvider>
       </I18nextProvider>
