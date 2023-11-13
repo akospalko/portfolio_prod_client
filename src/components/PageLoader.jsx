812c19e23bd0f2333793ef8c4dbcd3300515a52f
Agathius <p.akos1148@gmail.com>
2023-11-13 14:44:48 +0100

    added, component info, decreased loader size, removed unused syling

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,12 +1,17 @@
+// Full page loader 
 import React from 'react';
 import './Shared.css';
 import { BubbleLoader } from './SVGComponents';
 
 const PageLoader = ({ style }) => {
+  // STYLE
+  const iconSize = '150px';
+  const iconColor1 = 'var(--color_3_light)';
+  const iconColor2 = 'var(--color_1)';
 
   return (
-    <article className={ `shared-page-container ${ style }` }>
-      <BubbleLoader color1='var(--color_3_light)' color2='var(--color_1)'/>
+    <article className={ `shared-page-container ${ style }` } >
+      <BubbleLoader width={ iconSize } height={ iconSize } color1={ iconColor1 } color2={ iconColor2 } />
     </article>
   )
 }
